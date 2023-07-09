1f1a9deaf68aaf49b10fd8f80a2581248aab3926
ibbya42 <ibbyali4214@gmail.com>
2023-07-10 11:38:39 +1200

    10/7 let variable

--- app.js
+++ app.js
@@ -1,11 +1,27 @@
-// String  
-console.log('Ibrahim');
+// // String Data Type
+// console.log('Ibrahim')
 
-// String Indexing
-console.log('Hello World' [10]);
+// // String Indexing
+// console.log('Hello World' [10])
 
-// Finding String Length - Total length of characters in the string
-console.log('Ibrahim'.length)
-// Alternative Method
-let str = ('Ibrahim');
-console.log(str.length);
+// // Finding String Length - Total length of characters in the string
+// console.log('Ibrahim'.length)
+// // Alternative Method
+// let str = ('Ibrahim')
+// console.log(str.length)
+
+// // Number Data Type
+// console.log(5+5)
+
+// // Boolean Data Type
+// // Will return False
+// console.log(5 > 10)
+// // Will return True
+// console.log(5 < 10)
+
+let name = 'Ibrahim'
+let temperature = 15
+let planetName ='Mars'
+
+temperature = temperature + 5
+console.log(temperature)
