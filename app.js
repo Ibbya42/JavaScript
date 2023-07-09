5aeb03a5f529b2ebc205c2fa10ca058a38332728
ibbya42 <ibbyali4214@gmail.com>
2023-07-10 10:28:32 +1200

    10/7 Second Push

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
+// String  
+console.log('Ibrahim');
+
+// String Indexing
 console.log('Hello World' [10]);
+
+// Finding String Length - Total length of characters in the string
+console.log('Ibrahim'.length)
+// Alternative Method
+let str = ('Ibrahim');
+console.log(str.length);
