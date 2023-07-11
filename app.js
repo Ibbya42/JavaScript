4f858418a7874747192efd604cf6972ec753eea4
ibbya42 <ibbyali4214@gmail.com>
2023-07-11 12:55:34 +1200

    11/07 Push loops

--- app.js
+++ app.js
@@ -76,24 +76,29 @@
 // }
 
 
-for ( let i = 1 ; i <=20 ; i++){   
+// for ( let i = 1 ; i <=20 ; i++){   
 
-     if (i % 5 == 0 && i % 3 == 0) {
-        console.log('Frontend Simplified');
-    }
+//      if (i % 5 == 0 && i % 3 == 0) {
+//         console.log('Frontend Simplified');
+//     }
 
-   else if (i % 3 == 0) {
-        console.log('Frontend');
-    }
+//    else if (i % 3 == 0) {
+//         console.log('Frontend');
+//     }
 
-    else if (i % 5 == 0) {
-        console.log('Simplified');
-    }
+//     else if (i % 5 == 0) {
+//         console.log('Simplified');
+//     }
 
-    else{
-        console.log(i);
-    } 
+//     else{
+//         console.log(i);
+//     } 
 
+// }
+
+
+const str = 'Frontend Simplified'
+
+for ( let i = 0; i <19 ; i++){
+    console.log(str[i]);
 }
-
-
