c58d2f5edeaa4f6dba3a495b2c3d24ca0ae3985b
ibbya42 <ibbyali4214@gmail.com>
2023-07-25 12:12:42 +1200

    25/07 Push 1

--- app2.js
+++ app2.js
@@ -3,7 +3,7 @@
 
 // Created a constant variable called emailRef 
 // Created a document query selector to call the span call from the html
-// const emailRef = document.querySelector('.email')
+const emailRef = document.querySelector('.email')
 
 // // Two Methods to unlock data
 // // 1. Then 
@@ -31,8 +31,12 @@
         // To unlock the data we can use a variable which equal to the data that is being 
         // fetched. 
         const response = await fetch("https://jsonplaceholder.typicode.com/users/1")
+        // Unlock the data by turnng the response.json command into a variable then 
+        // calling it on a console.log
         const data = await response.json()
         console.log(data)
+        // call email ref to add email address onto .email on html fle
+        emailRef.innerHTML = data.email
         
         
     }
